39326fbfcd99913c8474eb906e63ce103dcf36e6
1711shashank <1711shashank@gmail.com>
2023-02-12 14:11:14 +0530

    completely responsive

--- src/Header.js
+++ src/Header.js
@@ -1,33 +1,26 @@
 import React from 'react'
-import NotificationsActiveOutlinedIcon from '@mui/icons-material/NotificationsActiveOutlined';
-import MoreVertOutlinedIcon from '@mui/icons-material/MoreVertOutlined';
 import './Header.css'
 
 const Header = () => {
     return (
-        <div className='header'>
-            <div className='header-up'>
-                <MoreVertOutlinedIcon/>
-                <input />
-                <NotificationsActiveOutlinedIcon />
-            </div>
-            <div className='header-down'>
-                <div className='header-down-left'>
-                    <img src="https://avatars.githubusercontent.com/u/34642119?v=4" height="70px" width="70px" />
-                    <div class="header-down-leftText">
+        <>
+            <div className='header-container'>
+                <div className='header-left'>
+                    <img src="https://avatars.githubusercontent.com/u/34642119?v=4" alt="" height="70px" width="70px" />
+                    <div class="header-leftText">
                         <p class="text-message">Hi there,</p>
                         <p class="text-nameId"><span>Kumar Shashank</span> (@shashank)</p>
                     </div>
                 </div>
 
-                <div className='header-down-right'>
-                    <button class="new-button">New</button>
+                <div className='header-right'>
+                    <button class="new-button"> New </button>
                     <button class="upload-button">Upload</button>
                     <button class="share-button">Share</button>
                 </div>
-            </div>
 
-        </div>
+            </div>
+        </>
     )
 }
 
